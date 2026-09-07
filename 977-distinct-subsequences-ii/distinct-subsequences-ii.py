102771fadc3317eeade2241a21633daa3332aeb2
VIVEK <vidyadavi644@gmail.com>
2026-09-07 22:25:52 +0530

    Time: 20 ms (19.79%) | Memory: 19.7 MB (9.37%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def solve(self, s: str, n: int, prev: list[int], memo: list[int]) -> int:
+        if n == 0:
+            return 1
+
+        if memo[n] != -1:
+            return memo[n]
+
+        total = 2 * self.solve(s, n - 1, prev, memo)
+
+        duplicate = 0
+
+        if prev[n] != -1:
+            duplicate = self.solve(s, prev[n] - 1, prev, memo)
+
+        memo[n] = (total - duplicate) % (10**9 + 7)
+
+        return memo[n]
+
+    def distinctSubseqII(self, s: str) -> int:
+        n = len(s)
+        MOD = 10**9 + 7
+
+        prev = [-1] * (n + 1)
+
+        last = {}
+
+        for i in range(1, n + 1):
+            c = s[i - 1]
+
+            if c in last:
+                prev[i] = last[c]
+
+            last[c] = i
+
+        memo = [-1] * (n + 1)
+
+        return (self.solve(s, n, prev, memo) - 1) % MOD
