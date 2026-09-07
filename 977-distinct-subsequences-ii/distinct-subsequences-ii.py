cf439d4d0781099212186b380caf1f5497726d0b
VIVEK <vidyadavi644@gmail.com>
2026-09-07 22:34:26 +0530

    Time: 10 ms (66.67%) | Memory: 19.5 MB (14.58%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.py
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.py
@@ -1,21 +1,21 @@
 class Solution:
-    def solve(self, s: str, n: int, prev: list[int], memo: list[int]) -> int:
-        if n == 0:
-            return 1
+    # def solve(self, s: str, n: int, prev: list[int], memo: list[int]) -> int:
+        # if n == 0:
+        #     return 1
 
-        if memo[n] != -1:
-            return memo[n]
+        # if memo[n] != -1:
+        #     return memo[n]
 
-        total = 2 * self.solve(s, n - 1, prev, memo)
+        # total = 2 * self.solve(s, n - 1, prev, memo)
 
-        duplicate = 0
+        # duplicate = 0
 
-        if prev[n] != -1:
-            duplicate = self.solve(s, prev[n] - 1, prev, memo)
+        # if prev[n] != -1:
+        #     duplicate = self.solve(s, prev[n] - 1, prev, memo)
 
-        memo[n] = (total - duplicate) % (10**9 + 7)
+        # memo[n] = (total - duplicate) % (10**9 + 7)
 
-        return memo[n]
+        # return memo[n]
 
     def distinctSubseqII(self, s: str) -> int:
         n = len(s)
@@ -33,6 +33,16 @@ class Solution:
 
             last[c] = i
 
-        memo = [-1] * (n + 1)
+        dp = [0] * (n + 1)
+        dp[0] = 1
 
-        return (self.solve(s, n, prev, memo) - 1) % MOD
+        for i in range(1,n+1):
+            total = (2*dp[i-1])%MOD
+
+            if prev[i] != -1:
+                duplicates = dp[prev[i]-1]
+                total = (total - duplicates + MOD)%MOD
+                
+            dp[i] = total
+
+        return (dp[n]-1 + MOD) % MOD
